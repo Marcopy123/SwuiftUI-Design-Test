726afa348e77a40419e505c051ebdbd0da111340
marco.py <marco.py@yahoo.com>
2023-12-28 16:24:30 -0500

    Initial Commit

---
+++ TestUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestUI
+//
+//  Created by Marco on 2023-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestUI/TestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUIApp.swift
+//  TestUI
+//
+//  Created by Marco on 2023-12-28.
+//
+
+import SwiftUI
+
+@main
+struct TestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
